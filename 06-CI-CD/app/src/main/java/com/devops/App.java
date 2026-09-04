ba113417a222d3a344f731ae61a4867803d6951c
madawaMax <madawavishwajith7@gmail.com>
2026-09-04 19:53:10 +0530

    DEVOPS-059: Test automatic pipeline trigger

--- 06-CI-CD/app/src/main/java/com/devops/App.java
+++ 06-CI-CD/app/src/main/java/com/devops/App.java
@@ -3,7 +3,7 @@ package com.devops;
 public class App {
 
     public static String getMessage() {
-        return "DEVOPS-054 Build Pipeline is working!";
+        return "DEVOPS-059 Full CI/CD Pipeline is working!!";
     }
 
     public static void main(String[] args) {
